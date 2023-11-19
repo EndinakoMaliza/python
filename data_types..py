222584e7464db77cffe3aa023e8e592a12cf0c6c
Ediakomaliza <viwedash@gmail.com>
2023-11-19 20:01:51 +0200

    more lines

--- data_types..py
+++ data_types..py
@@ -7,3 +7,26 @@ last = "Maliza"
 print(type(first))
 print(type(first) == str)
 print(isinstance(first, str))
+
+# constructor funtion
+# pizza = str("Pepperoni")
+# print(type(pizza))
+# print(type(pizza) = str)
+# print(isinstance(pizza, str))
+
+# Concatenation
+fullname = first + " " + last 
+print(fullname)
+
+fullname += "!"
+print(fullname)
+
+# Casting a number to a string
+decade = str(2010)
+print(type(decade))
+print(decade)
+
+statement = "I was born in the " + decade + "s."
+
+print(statement)
+# print("today's date is " + )
